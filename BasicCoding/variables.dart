b23613d5ac335e9ecc8443ef9213de7c919f21b9
Abir Chandra Das <abirchandradas@Abirs-MacBook-Air.local>
2025-01-05 02:46:26 +0600

    Add examples of Dart variables

---
+++ BasicCoding/variables.dart
@@ -0,0 +1,39 @@
+void main() {
+  // Integer variable
+  int age = 25;
+  print('Age: $age');
+
+  // Double variable
+  double height = 5.9;
+  print('Height: $height');
+
+  // String variable
+  String name = 'Abir';
+  print('Name: $name');
+
+  // Boolean variable
+  bool isStudent = true;
+  print('Is Student: $isStudent');
+
+  // List variable
+  List<String> colors = ['Red', 'Green', 'Blue'];
+  print('Colors: $colors');
+
+  // Map variable
+  Map<String, int> scores = {'Math': 90, 'Science': 85};
+  print('Scores: $scores');
+
+  // Dynamic variable
+  dynamic anything = 'Hello';
+  print('Dynamic variable: $anything');
+  anything = 123;
+  print('Dynamic variable: $anything');
+
+  // Constant variable
+  const double pi = 3.14159;
+  print('Pi: $pi');
+
+  // Final variable
+  final DateTime now = DateTime.now();
+  print('Current Date and Time: $now');
+}
