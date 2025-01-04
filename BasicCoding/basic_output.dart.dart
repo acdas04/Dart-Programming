8607eef0eee622cd01170167323966b112333a91
Abir Chandra Das <abirchandradas@Abirs-MacBook-Air.local>
2025-01-05 02:31:08 +0600

    first commit

---
+++ BasicCoding/basic_output.dart.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Hello World");
+  print(2 + 3);
+  // print(abir,das);
+  stdout.write('abir');
+  print(' das');
+  print('*' * 5);
+  print("abir das . id no ${38}");
+}
